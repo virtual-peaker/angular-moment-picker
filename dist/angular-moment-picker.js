7400977183cc03551e5772c49aed10e147bf0a66
Jorge Saucedo <jsaucedo@Naboo.local>
2022-01-07 13:46:49 -0500

    revert

--- dist/angular-moment-picker.js
+++ dist/angular-moment-picker.js
@@ -672,13 +672,18 @@ exports.getOffset = function (element) {
         return;
     if (!element.getClientRects().length)
         return { top: 0, left: 0 };
+    // https://github.com/jquery/jquery/blob/3.1.1/src/core.js#L220
+    var isWindow = function (obj) { return obj != null && obj === obj.window; };
+    var getWindow = function (elem) { return isWindow(elem) ? elem : elem.nodeType === 9 && elem.defaultView; }; // tslint:disable-line:no-any
     var rect = element.getBoundingClientRect();
     if (!rect.width && !rect.height)
         return rect;
-    var div = element.ownerDocument.querySelector('angular-middle-panel').shadowRoot.getElementById('vp-app');
+    var doc = element.ownerDocument;
+    var win = getWindow(doc);
+    var docElem = doc.documentElement;
     return {
-        top: rect.top + div.scrollTop,
-        left: rect.left + div.scrollLeft
+        top: rect.top + win.pageYOffset - docElem.clientTop,
+        left: rect.left + win.pageXOffset - docElem.clientLeft
     };
 };
 
--- src/helpers.ts
+++ src/helpers.ts
@@ -5,13 +5,19 @@ export const getOffset = (element: HTMLElement): { top: number, left: number } =
 	if (!element) return;
 	if (!element.getClientRects().length) return { top: 0, left: 0 };
 
+	// https://github.com/jquery/jquery/blob/3.1.1/src/core.js#L220
+	const isWindow = (obj: Window): boolean => obj != null && obj === obj.window;
+	const getWindow = (elem: any): Window => isWindow(elem) ? elem : elem.nodeType === 9 && elem.defaultView; // tslint:disable-line:no-any
+
 	let rect: ClientRect = element.getBoundingClientRect();
 	if (!rect.width && !rect.height) return rect;
 	
-	const div = element.ownerDocument.querySelector('angular-middle-panel').shadowRoot.getElementById('vp-app');
+	let doc: Document        = element.ownerDocument;
+	let win: Window          = getWindow(doc);
+	let docElem: HTMLElement = doc.documentElement;
 
 	return {
-		top: rect.top + div.scrollTop,
-		left: rect.left + div.scrollLeft,
+		top: rect.top + win.pageYOffset - docElem.clientTop,
+		left: rect.left + win.pageXOffset - docElem.clientLeft
 	};
 };
